e7dd28c9d3b9d746373b4d8b56e9dd2a884963a3
Dustin Ruetz <dustin.ruetz@agilebits.com>
2024-03-18 18:54:35 -0400

    feat(init.sh): print messages in present tense

--- init.sh
+++ init.sh
@@ -7,13 +7,13 @@ REPO_NAME="sandbox-go-webserver"
 
 printLine "ℹ️  Initializing $REPO_NAME..."
 
-printLine "1️⃣  Configure repo to use the source-controlled scripts in the .githooks/ directory..."
+printLine "1️⃣  Configuring repo to use the source-controlled scripts in the .githooks/ directory..."
 git config core.hooksPath ./.githooks/
 
-printLine "2️⃣  Install dependencies..."
+printLine "2️⃣  Installing dependencies..."
 go mod tidy
 
-printLine "3️⃣  Generate the initial build..."
+printLine "3️⃣  Generating the initial build..."
 make build
 
 printLine "✅ $REPO_NAME has been initialized."
